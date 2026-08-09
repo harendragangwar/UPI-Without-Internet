9311175d40b35c1b52dc658433e2cf0d7641a969
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-09 09:21:25 +0530

    appconfig

--- src/main/java/com/demo/upimesh/config/AppConfig.java
+++ src/main/java/com/demo/upimesh/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.demo.upimesh.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @Configuration
 @EnableScheduling
 public class AppConfig {
